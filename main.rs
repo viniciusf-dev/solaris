cc71864a50ab89fdbd7c6bbdc8a0d1d268fc87dc
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2025-05-08 02:07:52 -0300

    feat: uploading cargo file and main executor

---
+++ main.rs
@@ -0,0 +1,52 @@
+use std::error::Error;
+
+mod config;
+mod core;
+mod index;
+mod storage;
+mod types;
+mod utils;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Starting Solaris Vector Database...");
+    
+
+    let mut db = core::database::Database::new("solaris_test".to_string());
+    
+    let collection_name = "test_collection";
+    db.create_collection(collection_name, 384)?;
+    println!("Created collection '{}'", collection_name);
+    
+    let test_vectors = vec![
+        (
+            "doc1".to_string(),
+            vec![0.1, 0.2, 0.3; 128], 
+            Some(vec![("type".to_string(), "article".to_string())]),
+        ),
+        (
+            "doc2".to_string(),
+            vec![0.2, 0.3, 0.4; 128],
+            Some(vec![("type".to_string(), "video".to_string())]),
+        ),
+        (
+            "doc3".to_string(),
+            vec![0.3, 0.4, 0.5; 128],
+            Some(vec![("type".to_string(), "article".to_string())]),
+        ),
+    ];
+    
+    for (id, vector, metadata) in test_vectors {
+        db.insert_vector(collection_name, id, vector, metadata)?;
+    }
+    println!("Inserted test vectors into collection");
+    
+    let query_vector = vec![0.1, 0.2, 0.3; 128];
+    let results = db.search_vectors(collection_name, query_vector, 2)?;
+    
+    println!("Search results:");
+    for (id, score, metadata) in results {
+        println!("ID: {}, Score: {}, Metadata: {:?}", id, score, metadata);
+    }
+    
+    Ok(())
+}
