d9b024e69dfe7d7626eb78ea9de29d4ed1fa0671
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2025-06-09 17:55:29 -0300

    fix: fixing vector dimensions

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,27 @@ mod utils;
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Starting Solaris Vector Database...");
     
-
+    // Criar uma instância do database
     let mut db = core::database::Database::new("solaris_test".to_string());
     
     let collection_name = "test_collection";
-    db.create_collection(collection_name, 384)?;
+    db.create_collection(collection_name, 128)?;
     println!("Created collection '{}'", collection_name);
     
     let test_vectors = vec![
         (
             "doc1".to_string(),
-            vec![0.1, 0.2, 0.3; 128], 
+            vec![0.1; 128], 
             Some(vec![("type".to_string(), "article".to_string())]),
         ),
         (
             "doc2".to_string(),
-            vec![0.2, 0.3, 0.4; 128],
+            vec![0.2; 128], 
             Some(vec![("type".to_string(), "video".to_string())]),
         ),
         (
             "doc3".to_string(),
-            vec![0.3, 0.4, 0.5; 128],
+            vec![0.3; 128], 
             Some(vec![("type".to_string(), "article".to_string())]),
         ),
     ];
@@ -40,12 +40,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
     println!("Inserted test vectors into collection");
     
-    let query_vector = vec![0.1, 0.2, 0.3; 128];
+    let query_vector = vec![0.15; 128]; 
     let results = db.search_vectors(collection_name, query_vector, 2)?;
     
     println!("Search results:");
     for (id, score, metadata) in results {
-        println!("ID: {}, Score: {}, Metadata: {:?}", id, score, metadata);
+        println!("ID: {}, Score: {:.6}, Metadata: {:?}", id, score, metadata);
     }
     
     Ok(())
